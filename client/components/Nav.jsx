d9514c8d07a735aef2e4b6d6ef9501c799fe4129
Rachomir <bciach@thebrain.pro>
2016-01-27 13:53:22 +0100

    Changed navigation styles

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,12 +3,17 @@ Nav = React.createClass({
     render() {
         return (
             <div>
-                <nav className="navbar navbar-inverse">
+                <nav className="navbar navbar-inverse navbar-custom">
                     <div className="container-fluid">
                         <div className="navbar-default">
                             <ul className="nav navbar-nav navbar-left">
+                                <li><a href="#"><img id="myBrand" src="img/logo@2x.png"></img></a></li>
                                 <li><a href="#">Sign up!</a></li>
-                                <li><a href="#">Flashcards</a></li>
+                                <li className="dropdown">
+                                    <a className="dropdown-toggle" data-toggle="dropdown" href="#">Flashcards<span className="caret"></span></a>
+                                    <ul className="dropdown-menu"><li><a href="#">Look for Flashcards</a></li>
+                                    </ul>
+                                </li>
                                 <li><a href="#">Courses</a></li>
                                 <li><a href="#">KhanAcademy Playlists</a></li>
                             </ul>
